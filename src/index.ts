93a6ae72e52ee97e298900e4bc41283aae2e430c
sunmin <sunmin0406@naver.com>
2024-04-01 22:44:43 +0900

    fix: tsup으로 번들 변경

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,6 @@
+// import { getCurrentDrawRound, getLottoResult } from './result';
+
+// export { getCurrentDrawRound, getLottoResult };
+
 export * from './result';
 export * from './lotto.interface';
